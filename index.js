f94e4e301fcd7b173b731181c69bb0777ee66d60
Light <sagarofficial2@gmail.com>
2024-11-23 10:32:58 +0530

    In Logger excluded root route

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
-app.use(morgan('combined'));
+app.use(
+  morgan("combined", {
+    skip: skipLoggingForRoot,
+  })
+);
 const port = 3000;
 
 var admin = require("firebase-admin");
@@ -13,32 +17,31 @@ var admin = require("firebase-admin");
 var serviceAccount = require(process.env.FCM_SERVER_KEY);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
-
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("OK");
-})
+});
 
 // Import routes
-const propertyRoutes = require('./routes/properties');
-const roomRoutes = require('./routes/rooms');
-const tenantRoutes = require('./routes/tenants');
-const paymentRoutes = require('./routes/payments');
-const notificationRoutes = require('./routes/notifications');
-const utilityRoutes = require('./routes/utility');
+const propertyRoutes = require("./routes/properties");
+const roomRoutes = require("./routes/rooms");
+const tenantRoutes = require("./routes/tenants");
+const paymentRoutes = require("./routes/payments");
+const notificationRoutes = require("./routes/notifications");
+const utilityRoutes = require("./routes/utility");
 // Use routes
-app.use('/properties', propertyRoutes);
-app.use('/rooms', roomRoutes);
-app.use('/tenants', tenantRoutes);
-app.use('/payments', paymentRoutes);
-app.use('/notifications', notificationRoutes);
-app.use('/utility', utilityRoutes)
+app.use("/properties", propertyRoutes);
+app.use("/rooms", roomRoutes);
+app.use("/tenants", tenantRoutes);
+app.use("/payments", paymentRoutes);
+app.use("/notifications", notificationRoutes);
+app.use("/utility", utilityRoutes);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
